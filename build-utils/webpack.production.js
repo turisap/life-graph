1efa1d262994acfca881c6eea78108287192a3e0
turisap <kirillshakirov57@gmail.com>
2019-11-06 22:46:27 +0200

    webpack first build

---
+++ build-utils/webpack.production.js
@@ -0,0 +1,3 @@
+module.exports = () => ({
+  devtool: "nosource-source-map"
+});
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const webpack = require("webpack");
+const modeConfig = mode => require(`./build-utils/webpack.${mode}`)(mode);
+const webpackMerge = require("webpack-merge");
+const presetConfig = require("./build-utils/loadPresets");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// TODO webpack polyfill fo css (postcss) for prod
+// TODO webpack manifest for prod
+
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  return webpackMerge(
+    {
+      mode,
+      entry: "./src/index.tsx",
+      output: {
+        path: __dirname + "/public",
+        publicPath: "./public",
+        filename: "bundle.js"
+      },
+      resolve: {
+        extensions: [".tsx", ".ts", ".js", ".jsx"]
+      },
+      module: {
+        rules: [
+          {
+            enforce: "pre",
+            loader: "eslint-loader",
+            test: /\.js$/,
+            exclude: /node_modules/,
+            options: {
+              emitWarning: true
+            }
+          },
+          {
+            test: /\.(tsx|ts)?$/,
+            use: "ts-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.js$/,
+            loader: "babel-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: "dirname/[contenthash].[ext]"
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader",
+                options: { minimize: true }
+              }
+            ]
+          },
+          {
+            test: /\.jpe?g$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  limit: 5000
+                }
+              }
+            ]
+          },
+          {
+            test: /\.s?css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader
+              },
+              {
+                loader: "css-loader"
+              }
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+          filename: "[name].css",
+          chunkFilename: "[id].css",
+          hmr: process.env.NODE_ENV === "development"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebPackPlugin({
+          filename: "./index.html"
+        })
+      ],
+      devServer: {
+        contentBase: "./dist",
+        hot: true,
+        historyApiFallback: true,
+        port: 9000
+      }
+    },
+    modeConfig(mode),
+    presets ? presetConfig({ mode, presets }) : null
+  );
+};
